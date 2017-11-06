3f2ff41e37e3f54ee451621705d0d6a287e9dd23
togaerror <togaerror@gmail.com>
2017-11-06 19:18:59 +0900

    Raspberry Pi用クライアント(仮)

--- pyenv/ClientClass.py
+++ pyenv/ClientClass.py
@@ -13,19 +13,53 @@ class ClientClass:
         self.client.connect(self.addr)
 
     #ここにServerとのやりとりを書いていく
-    def c_start(self):
-        print('--Client Start--')
+    #Webアプリケーション用クライアント
+    def app_start(self):
+        print('--Web Application Client Start--')
         while True:
-            msg = '''{ "test" : "json" }'''
-            msg = msg.encode('utf-8')
-            self.client.sendall(msg)
-            msg = self.client.recv(self.max_size)
-            print(msg)
-            break
-        
-        self.client.close()
+            try:
+                msg = self.client.recv(self.max_size)
+                msg = msg.decode('utf-8')
+            except OSError:
+                break
+            
 
+                
+
+        self.client.close()
+    #Raspberry Pi用クライアント
+    def rasp_start(self):
+        win = ''
+        print('--Raspberry Pi Client Start--')
+        while True:
+            try:
+                msg = self.client.recv(self.max_size)
+                msg = msg.decode('utf-8')
+            except OSError:
+                break
+            
+            #1回目は2つ受け取る
+            if win == '':
+                winn = self.client.recv(self.max_size)
+                winn = win.decode('utf-8')
+            #Winとmsgの設定で投票を行う
+            #差分の大きい方が勝ち
+
+            #どちらが勝ったかを返す
+            if win < msg:
+                win = msg
+            msg = win.encode('utf-8')
+            self.client.sendall(msg)
+            
+            #全ての水槽での結果を受け取る
+            win = self.client.recv(self.max_size)
+            win = win.decode('utf-8')
+        self.client.close()
 
 if __name__ == '__main__':
     c_class = ClientClass()
-    c_class.c_start()
+    comand = input('Rasb or App')
+    if comand == 'Rasb':
+        c_class.app_start()
+    elif comand == 'App'
+        c_class.app_start()
