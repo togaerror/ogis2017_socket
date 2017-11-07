6ae7d62b7b667c112ccab2ba6328a58cafcf6a88
togaerror <togaerror@gmail.com>
2017-11-07 12:30:47 +0900

    UI mode

--- pyenv/ClientClass.py
+++ pyenv/ClientClass.py
@@ -7,18 +7,29 @@ import json
 win_keep = queue.Queue() #勝ち残った設定
 
 class ClientClass:
-    def __init__(self):
+    def __init__(self, mode):
         self.max_size = 1024
         self.s_host = 'localhost'
         self.s_port = 6789
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((self.s_host, self.s_port))
         #msgの受信を待つスレッド
-        self.handler_thread = threading.Thread(target = self.client_handler, args = (), daemon = True)
-        self.handler_thread.start()
-        try:
-            self.input_msg()
-        finally:
+        if mode == '0': #UI
+            print('UI mode')
+            self.handler_thread = threading.Thread(target = self.UI_handler, args = (), daemon = True)
+            self.handler_thread.start()
+            try:
+                #設定の追加
+                self.input_msg()
+            finally:
+                self.client.close()
+        elif mode == '1': #画像
+            print('screen mode')
+
+        elif mode == '2': #RaspberryPi
+            print('RaspberryPi mode')
+        else:
+            print('no mode')
             self.client.close()
 
     def input_msg(self):
@@ -31,6 +42,13 @@ class ClientClass:
                 break
             else:
                 self.client.send(msg.encode('utf-8'))
+
+    def UI_handler(self):
+        while True:
+            #投票結果の受け取り
+            msg = self.client.recv(self.max_size)
+            msg = msg.decode('utf-8')
+            print('msg:{}'.format(msg))
 
     def client_handler(self):
         while True:
@@ -46,24 +64,12 @@ class ClientClass:
                 print('win_msg:{}'.format(win_msg))
                 print('msg:{}'.format(msg))
                 win_keep.put(win_msg)
-                '''
-                #投票処理
-
-                #投票結果を返す
-                if win_msg < msg: #もし新しい設定になるなら
-                    win_msg = msg
-                self.client.senall(msg.encode('utf-8'))
-                #勝ち残った設定を受け取る
-                msg = self.client.recv(self.max_size)
-                msg = msg.decode('utf-8')
-                if win_keep.empty() != True:
-                    win_keep.get()
-                win_keep.put(msg)
-                '''
+                
             elif msg != None:
                 print('msg:{}'.format(msg))
             else:
                 self.client.close()
 
 if __name__ == '__main__':
-    c_class = ClientClass()
+    mode = input('mode:') #0, 1, 2
+    c_class = ClientClass(mode)
