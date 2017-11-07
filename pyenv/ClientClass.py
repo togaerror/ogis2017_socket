1d72134c7dffacc1b061e5a59d70e8eebc69d32f
togaerror <togaerror@gmail.com>
2017-11-07 11:57:39 +0900

    擬似的スタート

--- pyenv/ClientClass.py
+++ pyenv/ClientClass.py
@@ -1,9 +1,10 @@
 #-*- utf-8 -*-
 import socket
-import json
 import threading
-from datetime import datetime
-from time import sleep
+import queue
+import json
+
+win_keep = queue.Queue() #勝ち残った設定
 
 class ClientClass:
     def __init__(self):
@@ -34,8 +35,33 @@ class ClientClass:
     def client_handler(self):
         while True:
             msg = self.client.recv(self.max_size)
-            if msg != None:
-                print('msg:{}'.format(msg.decode('utf-8')))
+            msg = msg.decode('utf-8')
+            if msg == 'start': #投票スタートを受け取った場合
+                print('msg:{}'.format(msg))
+                #設定を待つ
+                win_msg = self.client.recv(self.max_size)
+                msg = self.client.recv(self.max_size)
+                win_msg.decode('utf-8')
+                msg.decode('utf-8')
+                print('win_msg:{}'.format(win_msg))
+                print('msg:{}'.format(msg))
+                win_keep.put(win_msg)
+                '''
+                #投票処理
+
+                #投票結果を返す
+                if win_msg < msg: #もし新しい設定になるなら
+                    win_msg = msg
+                self.client.senall(msg.encode('utf-8'))
+                #勝ち残った設定を受け取る
+                msg = self.client.recv(self.max_size)
+                msg = msg.decode('utf-8')
+                if win_keep.empty() != True:
+                    win_keep.get()
+                win_keep.put(msg)
+                '''
+            elif msg != None:
+                print('msg:{}'.format(msg))
             else:
                 self.client.close()
 
