2fbdbe29b66459aa511dc13b8a8b922a2815976a
togaerror <togaerror@gmail.com>
2017-11-07 10:25:48 +0900

    大規模変更

--- pyenv/ClientClass.py
+++ pyenv/ClientClass.py
@@ -1,65 +1,44 @@
+#-*- utf-8 -*-
 import socket
 import json
+import threading
 from datetime import datetime
 from time import sleep
 
 class ClientClass:
     def __init__(self):
         self.max_size = 1024
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s_host = 'localhost'
         self.s_port = 6789
-        self.addr = (self.s_host, self.s_port)
-        self.client.connect(self.addr)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.s_host, self.s_port))
+        #msgの受信を待つスレッド
+        self.handler_thread = threading.Thread(target = self.client_handler, args = (), daemon = True)
+        self.handler_thread.start()
+        try:
+            self.input_msg()
+        finally:
+            self.client.close()
 
-    #ここにServerとのやりとりを書いていく
-    #Webアプリケーション用クライアント
-    def app_start(self):
-        print('--Web Application Client Start--')
+    def input_msg(self):
+        #メッセージの入力とサーバへの送信
         while True:
-            try:
-                msg = self.client.recv(self.max_size)
-                msg = msg.decode('utf-8')
-            except OSError:
+            msg = input('>>>')
+            if msg == 'exit':
+                self.client.close()
+                print('close')
                 break
-            
+            else:
+                self.client.send(msg.encode('utf-8'))
 
-                
-
-        self.client.close()
-    #Raspberry Pi用クライアント
-    def rasp_start(self):
-        win = ''
-        print('--Raspberry Pi Client Start--')
+    def client_handler(self):
         while True:
-            try:
-                msg = self.client.recv(self.max_size)
-                msg = msg.decode('utf-8')
-            except OSError:
-                break
-            
-            #1回目は2つ受け取る
-            if win == '':
-                winn = self.client.recv(self.max_size)
-                winn = win.decode('utf-8')
-            #Winとmsgの設定で投票を行う
-            #差分の大きい方が勝ち
-
-            #どちらが勝ったかを返す
-            if win < msg:
-                win = msg
-            msg = win.encode('utf-8')
-            self.client.sendall(msg)
-            
-            #全ての水槽での結果を受け取る
-            win = self.client.recv(self.max_size)
-            win = win.decode('utf-8')
-        self.client.close()
+            msg = self.client.recv(self.max_size)
+            if msg != None:
+                print('msg:{}'.format(msg.decode('utf-8')))
+                print('>>>')
+            else:
+                self.client.close()
 
 if __name__ == '__main__':
     c_class = ClientClass()
-    comand = input('Rasb or App')
-    if comand == 'Rasb':
-        c_class.app_start()
-    elif comand == 'App'
-        c_class.app_start()
