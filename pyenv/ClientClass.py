427d9cb82f6b4fd6f3277afc80d78adbee54cce6
togaerror <togaerror@gmail.com>
2017-11-07 17:22:33 +0900

    関数かとこれから

--- pyenv/ClientClass.py
+++ pyenv/ClientClass.py
@@ -5,7 +5,7 @@ import queue
 import json
 import re
 
-win_keep = queue.Queue() #勝ち残った設定
+win_keep = ''
 
 class ClientClass:
     def __init__(self, mode):
@@ -29,7 +29,10 @@ class ClientClass:
             print('screen mode')
             self.handler_thread = threading.Thread(target = self.screen_handler, args = (mode), daemon = True)
             self.handler_thread.start()
-
+            try:
+                self.input_msg(mode)
+            finally:
+                self.client.close()
         elif mode == '2': #RaspberryPi
             print('RaspberryPi mode')
             self.handler_thread = threading.Thread(target = self.raspberry_handler, args = (mode), daemon = True)
@@ -55,39 +58,43 @@ class ClientClass:
                 self.client.send(mode.encode('utf-8')) 
                 self.client.send(msg.encode('utf-8'))
             elif mode == '2':
-                self.client.send(mdoe.encode('utf-8'))
+                self.client.send(mode.encode('utf-8'))
                 self.client.send(msg.encode('utf-8'))
+    
+    def recv_msgs(self):
+        msg_mode = self.client.recv(self.max_size)
+        msg_mode = msg_mode.decode('utf-8') 
+        msg = self.client.recv(self.max_size)
+        msg = msg.decode('utf-8')
+        return msg_mode, msg
 
-    def Raspberry_handler(self, mode):
+    def raspberry_handler(self, mode):
         while True:
-            msg_mode = self.client.recv(self.max_size)
-            msg_mode = mdoe.decode('utf-8') 
-            msg = self.client.recv(self.max_size)
-            msg = msg.decode('utf-8')
-            if msg_mode == mode:
-                print('msg:{}:'.formta(msg))
+            msg_mode, msg = self.recv_msgs()
+            if msg != None:
+                print('msg:{0} {1}'.format(msg_mode, msg))
+
+            else:
+                self.client.close()
 
     def screen_handler(self, mode):
         while True:
-            msg_mode = self.client.recv(self.max_size)
-            msg_mode = mdoe.decode('utf-8') 
-            msg = self.client.recv(self.max_size)
-            msg = msg.decode('utf-8')
-            if msg_mode == mode:
-                print('msg:{}'.format(msg))
-                
+            msg_mode, msg = self.recv_msgs()
+            if msg != None:
+                if msg_mode == mode:
+                    print('msg:{}'.format(msg))
+            else:
+                self.client.close()
 
-
-    def UI_handler(self):
+    def UI_handler(self, mode):
         while True:
             #投票結果の受け取り
-            msg_mode = self.client.recv(self.max_size)
-            msg_mode = msg.decode('utf-8') 
-            msg = self.client.recv(self.max_size)
-            msg = msg.decode('utf-8')
-            if mode == msg_mode:
-                print('msg:{}'.format(msg))                 
-                
+            msg_mode, msg = self.recv_msgs()
+            if msg != None:    
+                if mode == msg_mode:
+                    print('msg:{}'.format(msg))                 
+            else:
+                self.client.close()
 
 if __name__ == '__main__':
     mode = input('mode:') #0, 1, 2
