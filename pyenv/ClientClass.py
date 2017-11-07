abaaac2b10fc78f2c3034df05e5e6d3a54fdf2ef
togaerror <togaerror@gmail.com>
2017-11-07 13:06:34 +0900

    modeの実装

--- pyenv/ClientClass.py
+++ pyenv/ClientClass.py
@@ -3,6 +3,7 @@ import socket
 import threading
 import queue
 import json
+import re
 
 win_keep = queue.Queue() #勝ち残った設定
 
@@ -16,28 +17,33 @@ class ClientClass:
         #msgの受信を待つスレッド
         if mode == '0': #UI
             print('UI mode')
-            self.handler_thread = threading.Thread(target = self.UI_handler, args = (), daemon = True)
+            self.handler_thread = threading.Thread(target = self.UI_handler, args = (mode), daemon = True)
             self.handler_thread.start()
             try:
                 #設定の追加
-                self.input_msg()
+                self.input_msg(mode)
             finally:
                 self.client.close()
 
         elif mode == '1': #画像
             print('screen mode')
-            self.handler_thread = threading.Thread(target = self.screen_handler, args = (), daemon = True)
+            self.handler_thread = threading.Thread(target = self.screen_handler, args = (mode), daemon = True)
             self.handler_thread.start()
 
         elif mode == '2': #RaspberryPi
             print('RaspberryPi mode')
-            self.handler_thread = threading.Thread(target = self.raspberry_handler, args = (), daemon = True)
+            self.handler_thread = threading.Thread(target = self.raspberry_handler, args = (mode), daemon = True)
             self.handler_thread.start()
+            try:
+                #設定の追加
+                self.input_msg(mode)
+            finally:
+                self.client.close()
         else:
             print('no mode')
             self.client.close()
 
-    def input_msg(self):
+    def input_msg(self, mode):
         #メッセージの入力とサーバへの送信
         while True:
             msg = input()
@@ -45,22 +51,43 @@ class ClientClass:
                 self.client.close()
                 print('close')
                 break
-            else:
+            elif mode == '0':
+                self.client.send(mode.encode('utf-8')) 
                 self.client.send(msg.encode('utf-8'))
-    def raspberry_handler(self):
+            elif mode == '2':
+                self.client.send(mdoe.encode('utf-8'))
+                self.client.send(msg.encode('utf-8'))
+
+    def Raspberry_handler(self, mode):
         while True:
-            
-    def screen_handler(self):
-        while True:
+            msg_mode = self.client.recv(self.max_size)
+            msg_mode = mdoe.decode('utf-8') 
             msg = self.client.recv(self.max_size)
             msg = msg.decode('utf-8')
+            if msg_mode == mode:
+                print('msg:{}:'.formta(msg))
+
+    def screen_handler(self, mode):
+        while True:
+            msg_mode = self.client.recv(self.max_size)
+            msg_mode = mdoe.decode('utf-8') 
+            msg = self.client.recv(self.max_size)
+            msg = msg.decode('utf-8')
+            if msg_mode == mode:
+                print('msg:{}'.format(msg))
+                
+
 
     def UI_handler(self):
         while True:
             #投票結果の受け取り
+            msg_mode = self.client.recv(self.max_size)
+            msg_mode = msg.decode('utf-8') 
             msg = self.client.recv(self.max_size)
             msg = msg.decode('utf-8')
-            print('msg:{}'.format(msg))
+            if mode == msg_mode:
+                print('msg:{}'.format(msg))                 
+                
 
 if __name__ == '__main__':
     mode = input('mode:') #0, 1, 2
