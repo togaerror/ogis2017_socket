a0d04a535fadad4dd86302bc54991d7fd65387b5
togaerror <togaerror@gmail.com>
2017-11-11 17:34:12 +0900

    handerからlisnerへ

--- pyenv/webserver_merged/ClientClass.py
+++ pyenv/webserver_merged/ClientClass.py
@@ -13,16 +13,16 @@ class ClientClass:
         self.s_port = 6789
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((self.s_host, self.s_port))
+        if mode == '2':
+           self.raspberry_lisner() 
+        """
         #msgの受信を待つスレッド
         if mode == '0': #UI
             self.handler_thread = threading.Thread(target = self.UI_handler, args = (), daemon = True)
             self.handler_thread.start()
         elif mode == '1': #画像
-            '''
             self.handler_thread = threading.Thread(target = self.screen_handler, args = (), daemon = True)
             self.handler_thread.start()
-            '''
-            self.scleen_handler()
         elif mode == '2': #RaspberryPi
             '''
             self.handler_thread = threading.Thread(target = self.raspberry_handler, args = (), daemon = True)
@@ -32,9 +32,8 @@ class ClientClass:
         else:
             print('no mode')
             self.client.close()
-    
-    def UI_handler(self): #0
-        print('UI mode')
+        """
+    def UI_lisner(self):
         while True:
             msg = self.recv_msgs()
             if msg != None:
@@ -43,63 +42,93 @@ class ClientClass:
                 self.client.close()
             
             if msg != None:
-                print(msg)
                 if re.match('0:', msg):
                     msg = msg.lstrip('0:')
-                    print(msg)
-                    return msg
-
-    def screen_handler(self): #1
-        print('screen mode')
+                    return msg 
+    '''
+    def UI_handler(self): #0
         while True:
             msg = self.recv_msgs()
             if msg != None:
                 pass
-                if re.match('1:', msg):
-                    msg = msg.lstrip('1:')
-                    message_listen(msg)
-                    
-    def raspberry_handler(self): #2
-        print('raspberry mode')
-        while True:
-            msg = self.recv_msgs()
-            if msg != None:
-                pass
-                if re.match('2:', msg):
-                    msg = msg.lstrip('2:')
-                    print(msg)
-                    if re.search('@', msg):
-                        conf = msg.split('@')
-                        print('left:  {}'.format(conf[0]))
-                        print('right: {}'.format(conf[1]))
-                        print('--start--')
-                        #ここにRaspberryPIの処理を書いていく
-                        print('--end--')
-                        #RaspberryPIの処理が終わったらServerに結果を返す
-                        result = vote_sample.Vote() 
-                        print(result)
-                        self.client.sendall(result.encode('utf-8'))
-
-    def input_msg(self, mode):
-        #メッセージの入力とサーバへの送信
-        while True:
-            msg = input()
-            if msg == 'exit':
+            else:
                 self.client.close()
-                print('close')
-                break
-            elif mode == '0':
-                print('mode 0: send')
-                msg = mode + ':' + msg
-                self.client.sendall(msg.encode('utf-8'))
-            elif mode == '1':
-                msg = mode + ':' + msg
-                self.client.sendall(msg.encode('utf-8'))
-            elif mode == '2':
-                print('mode 2: send')
-                msg = mode + ':' + msg
-                self.client.sendall(msg.encode('utf-8'))
-    
+            
+            if msg != None:
+                if re.match('0:', msg):
+                    msg = msg.lstrip('0:')
+                    return msg
+    '''
+    def screen_lisner(self): #1
+        while True:
+            msg = self.recv_msgs()
+            if msg != None:
+                pass
+            else:
+                self.client.close()
+
+            if re.match('1:', msg):
+                msg = msg.lstrip('1:')
+                return msg
+    '''
+    def screen_handler(self): #1
+        while True:
+            msg = self.recv_msgs()
+            if msg != None:
+                pass
+            else:
+                self.client.close()
+
+            if re.match('1:', msg):
+                msg = msg.lstrip('1:')
+                return msg
+    '''
+    def raspberry_handler(self): #2
+        while True:
+            msg = self.recv_msgs()
+            if msg != None:
+                pass
+            else:
+                self.client.close()
+            
+            if re.match('2:', msg):
+                msg = msg.lstrip('2:')
+                if re.search('@', msg):
+                    conf = msg.split('@')
+                    print('left:  {}'.format(conf[0]))
+                    print('right: {}'.format(conf[1]))
+                    print('--start--')
+                    #ここにRaspberryPIの処理を書いていく
+                    self.client.sendall('screen0'.encode('utf-8'))
+                    print('--end--')
+                    #RaspberryPIの処理が終わったらServerに結果を返す
+                    result = vote_sample.Vote() 
+                    self.client.sendall(result.encode('utf-8'))
+                    print('--vote end--') 
+    '''
+    def raspberry_handler(self): #2
+        while True:
+            msg = self.recv_msgs()
+            if msg != None:
+                pass
+            else:
+                self.client.close()
+            
+            if re.match('2:', msg):
+                msg = msg.lstrip('2:')
+                if re.search('@', msg):
+                    conf = msg.split('@')
+                    print('left:  {}'.format(conf[0]))
+                    print('right: {}'.format(conf[1]))
+                    print('--start--')
+                    #ここにRaspberryPIの処理を書いていく
+                    self.client.sendall('screen0'.encode('utf-8'))
+                    print('--end--')
+                    #RaspberryPIの処理が終わったらServerに結果を返す
+                    result = vote_sample.Vote() 
+                    self.client.sendall(result.encode('utf-8'))
+                    print('--vote end--')
+    '''
     #msg受け取り
     def recv_msgs(self):
         msg = self.client.recv(self.max_size)
